a908830e9508373df25a835150ffdb9e420025a6
maysolivan <maysam.solaymani@gmail.com>
2023-07-04 08:50:16 +0330

    add day02 and variables

---
+++ Day 02/variables.py
@@ -0,0 +1,40 @@
+
+# Variables in Python
+
+first_name = 'Maysam'
+last_name = 'Solaymani'
+country = 'Iran'
+city = 'Amol'
+age = 38
+is_married = True
+skills = ['Java', 'SQL', 'C#', 'Python']
+person_info = {
+    'firstname':'Maysam', 
+    'lastname':'Solaymani', 
+    'country':'Iran',
+    'city':'Amol'
+    }
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name: ', last_name)
+print('Last name length: ', len(last_name))
+print('Country: ', country)
+print('City: ', city)
+print('Age: ', age)
+print('Married: ', is_married)
+print('Skills: ', skills)
+print('Person information: ', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Maysam', 'Solaymani', 'Iran', 38, True
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
