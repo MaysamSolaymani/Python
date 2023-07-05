f669780cfd950e407dfd8cca95672c81fa06969f
maysolivan <maysam.solaymani@gmail.com>
2023-07-05 09:00:59 +0330

    add openai

---
+++ OpenAI/main.py
@@ -0,0 +1,15 @@
+import openai
+
+openai.organization = "org-Pd75www91CqOGH1dCT4w2lyw"
+openai.api_key = "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"
+response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Q: Who is Batman?",
+    temperature=0,
+    max_tokens=64,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=["\n\n"]
+)
+print(response)
