e50b249b983f6bb6daf497910a9e570176f8e2ad
Meysam Soleymani <96109623+MaysamSolaymani@users.noreply.github.com>
2023-11-16 09:05:57 +0330

    Update UploadPost.py

--- Instagram/Instabot/UploadPost.py
+++ Instagram/Instabot/UploadPost.py
@@ -2,6 +2,7 @@ from instabot import Bot
 import os,datetime
 
 bot = Bot()
+#Initiate bot
 bot.login(username = "username", password = "password")
 mg = open(os.path.dirname(__file__) + "\\Caption.txt", "r")
 img = open(os.path.dirname(__file__) + "\\image.jpg", 'rb')
