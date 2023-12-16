94e2ae78e4542646bf62182a1bffa066af903890
maysolivan <maysam.solaymani@gmail.com>
2023-12-16 15:02:21 +0330

    add exercise files

---
+++ CodeBundle/Section4/4.4 Remote SQL/VerifyRemote.py
@@ -0,0 +1,16 @@
+import pyodbc
+import pandas as pd
+
+server = 'den1.mssql5.gear.host'
+db = 'testmenow'
+usr = 'testmenow'
+psword = 'TEST!!'
+
+
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';'
+                                                                              'DATABASE='+db+';UID='+usr+';PWD=' +  psword)
+c = cnxn.cursor()
+
+print(pd.read_sql_query("SELECT * FROM MySandwiches", cnxn))
+
+cnxn.close()
---
+++ CodeBundle/Section4/4.4 Remote SQL/remote.py
@@ -0,0 +1,26 @@
+import pyodbc
+
+server = 'den1.mssql5.gear.host'
+db = 'testmenow'
+usr = 'testmenow'
+psword = 'TEST!!'
+
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};'
+                      'SERVER='+server+';DATABASE='+db+';UID='+usr+
+                      ';PWD=' + psword)
+c = cnxn.cursor()
+
+c.execute('CREATE TABLE MySandwiches (meat TEXT, name TEXT, '
+          'bread TEXT, my_charge INTEGER, my_cost REAL)')
+
+
+menu = [
+    ("Roast Beef", "Roasted B", "white", 10, 5.5),
+    ("A", "Loaded Veggie", "wheat", 5, 2.5),
+    ('B', "Hammy Lamby","white", 12, 7.5)
+]
+c.executemany('INSERT INTO MySandwiches(meat, name, bread, my_charge, '
+              'my_cost) VALUES(?,?,?,?,?)', menu)
+
+cnxn.commit()
+cnxn.close()
