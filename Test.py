ccd20fe6ce530536fb0a94ce2a5ca76dc7cf2903
maysolivan <maysam.solaymani@gmail.com>
2023-12-16 14:23:48 +0330

    new command

--- Test.py
+++ Test.py
@@ -1,4 +1,8 @@
-ls = [1,2,5,4,3]
-print(ls)
-for item in ls:
-    print(item)
+def f():
+    ls = [1,2,5,4,3]
+    print(ls)
+    for item in ls:
+        assert item / 2
+
+if __name__ == "__main__":
+    f()
