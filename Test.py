58aabb68d091e3d307a9fbb77f3627bc96f54f77
maysolivan <maysam.solaymani@gmail.com>
2023-12-19 11:40:16 +0330

    one line if

--- Test.py
+++ Test.py
@@ -1,11 +1,19 @@
-print('A')
 
 def main():
     print('B')
 
-print('C')
+def t():
+    print('Bwww')
 
 if __name__ == "__main__":
     main()
-    
-print('D')
+    t()
+
+val = 1
+print(val)
+del val
+del t
+#t()
+
+a, b = 1,2
+print('a is greater than b' if a > b else 'a is less than b or equal b')
