7b87675e5d6b370aa778e4c25302b25d960ac303
maysolivan <maysam.solaymani@gmail.com>
2023-12-25 08:28:50 +0330

    plot py

--- Drowing.py
+++ Drowing.py
@@ -1,9 +1,5 @@
 import matplotlib
 from matplotlib import pyplot as plt
 plt.figure()
-plt.plot(1,2)
-plt.show()
-matplotlib.get_backend()
-plt.interactive(False)
-plt.figure()
+plt.plot()
 plt.show()
