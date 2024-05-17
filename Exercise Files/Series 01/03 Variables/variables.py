49e8729e5e6a72252690d0c4c4719f2fe0e94f83
MaysamSolaymani <Maysam.Solaymani@gmail.com>
2024-05-17 17:27:06 +0430

    changed

--- Exercise Files/Series 01/03 Variables/variables.py
+++ Exercise Files/Series 01/03 Variables/variables.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-# variables.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
-
 def main():
-    print("This is the variables.py file.")
+    x = [1,5,8,6,7]
+    x.append(9)
+    x.insert(0,'t')
+    print(type(x), x)
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
