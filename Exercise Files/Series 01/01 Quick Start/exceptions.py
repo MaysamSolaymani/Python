af89c8780e1025b24cbeba1bed64210a4e37db7c
MaysamSolaymani <Maysam.Solaymani@gmail.com>
2024-05-17 16:15:08 +0430

    changed

--- Exercise Files/Series 01/01 Quick Start/exceptions.py
+++ Exercise Files/Series 01/01 Quick Start/exceptions.py
@@ -1,9 +1,4 @@
-#!/usr/bin/python3
-# exceptions.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Gorup, LLC
-
-fh = open('xlines.txt')
+fh = open('lines.txt')
 for line in fh.readlines():
-    print(line)
+    print(line, end='')
 
--- Exercise Files/Series 01/01 Quick Start/whileloop.py
+++ Exercise Files/Series 01/01 Quick Start/whileloop.py
@@ -1,7 +1,3 @@
-#!/usr/bin/python3
-
-# simple fibonacci series
-# the sum of two elements defines the next set
 a, b = 0, 1
 while b < 50:
     print(b)
