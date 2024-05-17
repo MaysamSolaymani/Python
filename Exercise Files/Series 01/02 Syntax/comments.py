68d9fe94b60b44fe081350103c77aeef18e37858
MaysamSolaymani <Maysam.Solaymani@gmail.com>
2024-05-17 16:45:07 +0430

    changed

--- Exercise Files/Series 01/02 Syntax/comments.py
+++ Exercise Files/Series 01/02 Syntax/comments.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-# comments.py by Bill Weinman [http://bw.org/]
-# This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
-
 def main():
     for n in primes():
         if n > 100: break
