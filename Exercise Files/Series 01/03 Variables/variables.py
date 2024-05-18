11ecc38905887d911d4758a0ffc9c1b9add5b963
MaysamSolaymani <Maysam.Solaymani@gmail.com>
2024-05-18 10:55:08 +0430

    changed

--- Exercise Files/Series 01/03 Variables/variables.py
+++ Exercise Files/Series 01/03 Variables/variables.py
@@ -3,6 +3,9 @@ def main():
     x.append(9)
     x.insert(0,'t')
     print(type(x), x)
+    dic = {'first': 1, 'second': 2, 'third':3, 'forth':4}
+    for key in dic:
+        print(key, dic[key])
 
 if __name__ == "__main__":
     main()
