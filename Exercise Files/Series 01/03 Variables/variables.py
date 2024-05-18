50bd8527c68d40ce61d6fe6e4863f2090a4caab8
MaysamSolaymani <Maysam.Solaymani@gmail.com>
2024-05-18 10:59:18 +0430

    changed

--- Exercise Files/Series 01/03 Variables/variables.py
+++ Exercise Files/Series 01/03 Variables/variables.py
@@ -6,6 +6,12 @@ def main():
     dic = {'first': 1, 'second': 2, 'third':3, 'forth':4}
     for key in dic:
         print(key, dic[key])
+    d = dict(one=1, two=2, three=3, four=4, five=5)
+    for k in sorted(d):
+        print(k, d[k])
+    d['sex'] = 6
+    for k in sorted(d):
+        print(k, d[k])
 
 if __name__ == "__main__":
     main()
