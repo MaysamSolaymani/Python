84b5d1f6a494546d19bbda17a1c3d07cb2792208
MaysamSolaymani <Maysam.Solaymani@gmail.com>
2024-05-19 09:45:02 +0430

    changed

--- Exercise Files/Series 01/10 Classes/classes.py
+++ Exercise Files/Series 01/10 Classes/classes.py
@@ -1,12 +1,15 @@
 class Duck:
+    def __init__(self, value):
+        self.val = value
+
     def quack(self):
-        print('Quaaack!')
+        print('Quaaack! {}'.format(self.val))
 
     def walk(self):
-        print('Walks like a duck.')
+        print('Walks like a duck. {}'.format(self.val))
 
 def main():
-    donald = Duck()
+    donald = Duck(50)
     donald.quack()
     donald.walk()
 
