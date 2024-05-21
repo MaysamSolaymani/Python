28576c3988958f0798f56a8b1944c3b3efe592d9
MaysamSolaymani <Maysam.Solaymani@gmail.com>
2024-05-21 20:00:28 +0430

    decorator added

---
+++ Exercise Files/Series 02/Decorator.py
@@ -0,0 +1,12 @@
+def decorator(func):
+    def changefunc():
+        print('new string before change \'{}\''.format(func.__name__))
+        func()
+        print('\'{}\' changed'.format(func.__name__))
+    return changefunc
+
+@decorator
+def func():
+    print('test decorator')
+
+func()
