8948d27e3d457f2e85b6343ba0f2a0ff21d966ba
Meysam Soleymani <96109623+MaysamSolaymani@users.noreply.github.com>
2024-06-07 11:20:02 +0330

    Update variables.py

--- Exercise Files/Series 01/03 Variables/variables.py
+++ Exercise Files/Series 01/03 Variables/variables.py
@@ -6,6 +6,7 @@ def main():
     dic = {'first': 1, 'second': 2, 'third':3, 'forth':4}
     for key in dic:
         print(key, dic[key])
+        
     d = dict(one=1, two=2, three=3, four=4, five=5)
     for k in sorted(d):
         print(k, d[k])
