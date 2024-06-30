12d7a1363c5ff82698c013bb862b59c643dc4c04
Meysam Soleymani <96109623+MaysamSolaymani@users.noreply.github.com>
2024-06-30 12:51:12 +0330

    Update files.py

--- Exercise Files/Series 01/13 Files/files.py
+++ Exercise Files/Series 01/13 Files/files.py
@@ -1,12 +1,12 @@
 def main():
-    buffersize = 50000
+    buffersize = 40000
     fin = open('lines.txt', 'r')
     fout = open('new.txt', 'w')
     buffer = fin.read(buffersize)
     while len(buffer):
         fout.write(buffer)
         buffer = fin.readlines(buffersize)
-    buffersize = 50000
+    buffersize = 40000
     fin = open('olives.jpg', 'rb')
     fout = open('newolives.jpg', 'wb')
     buffer = fin.read(buffersize)
